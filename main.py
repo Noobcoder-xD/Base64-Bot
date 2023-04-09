7b5f6f8d788126ecbbb70caa767faf4166dd8ef9
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-04-09 14:25:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,13 +58,13 @@ def _filter(cmd: str):
    return filters.private & filters.incoming & filters.command(cmd)
 
 
-async def encode(string):
+async def B64encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(string_bytes)
     base64_string = (base64_bytes.decode("ascii")).strip("=")
     return base64_string
 
-async def decode(base64_string):
+async def B64decode(base64_string):
     base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
     string_bytes = base64.urlsafe_b64decode(base64_bytes) 
@@ -92,22 +92,22 @@ async def coder_(RiZoeL, message, type):
           return
     elif type == "encode":
        try:
-          encode = await encode(code)
+          encode = await B64encode(code)
           final_text = f"""
 **Encoded ✓**
 
-Encode: `{encode}=`
+**Encode:** `{encode}=`
 """
        except Exception as error:
           await message.reply(f"oops error! {error}")
           return
     elif type == "decode":
        try:
-          decode = await decode(code)
+          decode = await B64decode(code)
           final_text = f"""
 **Decoded ✓**
 
-Decode: `{decode}`
+**Decode:** `{decode}`
 """
        except Exception as error:
           await message.reply(f"oops error! {error}")
